7930bd43ec9d6ca6f1fc8a9d2dc1372957062bc1
AleksZyro <178569539+AleksZyro@users.noreply.github.com>
2026-08-03 23:47:35 +0200

    Bust PathLab logo cache

--- src/components/PathLabLogo.jsx
+++ src/components/PathLabLogo.jsx
@@ -1,7 +1,7 @@
 export default function PathLabLogo() {
   return (
     <div className="pathlab-logo" aria-hidden="true">
-      <img src={`${import.meta.env.BASE_URL}favicon.svg`} alt="" />
+      <img src={`${import.meta.env.BASE_URL}favicon.svg?v=2026-08-03-grid-logo`} alt="" />
     </div>
   );
 }
